66ea7ec0e6f5fec1fe53fbef811552543ebd49fe
Samy Pessé <samypesse@gmail.com>
2014-03-31 22:38:37 -0700

    Add gruntfile for building using GitBook

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+var path = require("path");
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-gitbook');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.initConfig({
+        'gitbook': {
+            development: {
+                dest: path.join(__dirname, ".grunt/gitbook"),
+                input: "./",
+                title: "How to Make a Computer Operating System in C++",
+                github: "SamyPesse/How-to-Make-a-Computer-Operating-System"
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: '.grunt/gitbook'
+            },
+            src: ['**']
+        },
+        'clean': {
+            files: '.grunt'
+        }
+    });
+
+    grunt.registerTask('publish', [
+        'gitbook',
+        'gh-pages',
+        'clean'
+    ]);
+    grunt.registerTask('default', 'gitbook');
+};
