0d07255d7ed20450547077c9bf575202e12cc18b
theclockknowsall <theclockknowsall@gmail.com>
2014-07-27 13:23:40 +0800

    first commit

--- src/sdk/diskimage.sh
+++ src/sdk/diskimage.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+# I added some delays and some root functions.
 qemu-img create c.img 2M
+sleep 1s
 fdisk ./c.img  << EOF
 x
 c
@@ -18,19 +21,25 @@ a
 1
 w
 EOF
+sleep 1s
 fdisk -l -u ./c.img
-losetup -o 32256 /dev/loop1 ./c.img
-
-mke2fs /dev/loop1
-mount  /dev/loop1 /mnt/
-cp -R bootdisk/* /mnt/
-umount /mnt/
-grub --device-map=/dev/null << EOF
+sleep 3s
+sudo losetup -o 32256 /dev/loop1 ./c.img
+sleep 3s
+sudo mke2fs /dev/loop1
+sleep 1s
+sudo mount  /dev/loop1 /mnt/
+sleep 1s
+sudo cp -R bootdisk/* /mnt/
+sleep 1s
+sudo umount /mnt/
+sleep 1s
+sudo grub --device-map=/dev/null << EOF
 device (hd0) ./c.img
 geometry (hd0) 4 16 63
 root (hd0,0)
 setup (hd0)
 quit
 EOF
-
-losetup -d /dev/loop1
+sleep 1s
+sudo losetup -d /dev/loop1
